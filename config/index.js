c38baec4cb7a68843451a5dfeb4119ad6250c3f7
Tolleiv Nietsch <info@tolleiv.de>
2012-03-01 23:59:06 +0100

    Add dummy config file

---
+++ config/index.js
@@ -0,0 +1,27 @@
+
+var twitter = {
+    consumerKey: '',
+    consumerSecret: '',
+    baseURL: 'http://localhost:3000',
+//    logging: true, // If true, uses winston to log.
+    afterLogin: '/login'
+};
+
+module.exports.twitter = twitter;
+
+module.exports.user_credentials = function(user) {
+    return {
+        consumer_key: twitter.consumerKey,
+        consumer_secret: twitter.consumerSecret,
+        access_token_key: user.token,
+        access_token_secret: user.tokenSecret,
+    }
+};
+module.exports.userCredentials = function(user) {
+    return {
+        consumerKey: twitter.consumerKey,
+        consumerSecret: twitter.consumerSecret,
+        accessTokenKey: user.token,
+        accessTokenSecret: user.tokenSecret,
+    }
+};
