b6989324f79018ba4f6afa078661d8359c88358e
Tolleiv Nietsch <info@tolleiv.de>
2012-03-02 02:22:32 +0100

    Adding some text to the "hello" page - to explain a bit what the service is about etc….

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,8 +8,9 @@ $(document).ready(function () {
         },
         template:function (response) {
             var markup = '';
+            console.log(response[0]);
             for (var i = 0; i < response.length; i++) {
-                markup += '<div class="tweet">' + response[i].summary + '</div>';
+                markup += '<div class="tweet">' + renderTweet(response[i].data) + '</div>';
             }
             return markup;
         },
@@ -23,3 +24,15 @@ $(document).ready(function () {
         expiration:1
     });
 });
+
+function renderTweet(tweet) {
+    var text = tweet.text;
+    var linkTpl = '<a href="URL">LINK</a>';
+    for(var i=0;i<tweet.entities.urls.length;i++) {
+        var c = tweet.entities.urls[i];
+        console.log(c)
+        var link = linkTpl.replace(/URL/, c.expanded_url).replace(/LINK/, c.display_url)
+        text = text.replace(c.url, link);
+    }
+    return text + " &mdash; <span class='at-user'>@" +  tweet.user.screen_name+ "</span>";
+}
