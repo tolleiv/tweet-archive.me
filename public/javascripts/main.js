649923e4185337393cbc9b02a66a511696cae92c
Tolleiv Nietsch <info@tolleiv.de>
2012-03-02 18:26:23 +0100

    Make the actual URLs visible in the timeline as well

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -18,9 +18,10 @@ $(document).ready(function () {
             return response.length;
         },
         offset:0,
-        max:100,
+        //max:100,
         loader:'<div class="loader"></div>',
         useCache:false,
+        sensitivity: 250,
         expiration:1
     });
 });
@@ -30,8 +31,11 @@ function renderTweet(tweet) {
     var linkTpl = '<a href="URL">LINK</a>';
     for(var i=0;i<tweet.entities.urls.length;i++) {
         var c = tweet.entities.urls[i];
-        console.log(c)
-        var link = linkTpl.replace(/URL/, c.expanded_url).replace(/LINK/, c.display_url)
+        var display_url = c.expanded_url.replace(/^https?:\/\//,'').replace(/^www/,'');
+        if (display_url.length > 40) {
+            display_url = display_url.substr(0,40) + '...'
+        }
+        var link = linkTpl.replace(/URL/, c.expanded_url).replace(/LINK/, display_url)
         text = text.replace(c.url, link);
     }
     return text + " &mdash; <span class='at-user'>@" +  tweet.user.screen_name+ "</span>";
