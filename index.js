57296500f19f69d4926b2b0995d57ac67c083a14
Tolleiv Nietsch <info@tolleiv.de>
2012-03-07 23:12:04 +0100

    Make sure we've no duplicates

--- index.js
+++ index.js
@@ -12,7 +12,42 @@ var refresh = setInterval(function () {
 
 function indexNewDocs() {
     var client = solr.createClient(config.solr);
-    MessageModel.find({ indexed: 0}).populate('users').run(function (err, docs) {
+
+    MessageModel.find({indexed:0}, function(err, docs) {
+        docs.forEach(function(doc) {
+            MessageModel.find({'data.id':doc.data.id}, function(err,messages) {
+                var newDoc = doc;
+                if (messages.length == 1) {
+                    newDoc.indexed = 1;
+                    newDoc.save();
+                } else if (messages.length > 2) {
+                    var indexedDoc = null;
+                    messages.forEach(function(msg) {
+                        indexedDoc = (!indexedDoc && msg.indexed == 2) ? msg : indexedDoc;
+                    });
+                    indexedDoc = indexedDoc || newDoc
+                    messages.forEach(function(msg) {
+                        if (msg._id == indexedDoc._id) { return;}
+                        indexedDoc.users = indexedDoc.users.concat(msg.users).unique();
+                        msg.indexed = 5
+                        msg.save();
+                    })
+                    indexedDoc.indexed = 1
+                    indexedDoc.save();
+                } else {
+                    var oldDoc = messages[0]._id == newDoc._id ? messages[1] : messages[0];
+                    oldDoc.users = oldDoc.users.concat(newDoc.users).unique();
+                    oldDoc.indexed = 1;
+                    oldDoc.save();
+                        // deleted but not (yet) removed
+                    newDoc.indexed = 5;
+                    newDoc.save();
+                }
+            });
+        })
+    });
+
+    MessageModel.find({ indexed: 1}).populate('users').run(function (err, docs) {
         if (err) { console.error(err); return; }
         docs.forEach(function (doc) {
             var mongoDoc = doc;
@@ -30,7 +65,7 @@ function indexNewDocs() {
                 }
                 client.add(solrDoc, function (err) {
                     if (err) return;
-                    mongoDoc.indexed = 1;
+                    mongoDoc.indexed = 2;
                     mongoDoc.save(function (err, doc) {
                     });
                 });
@@ -40,3 +75,11 @@ function indexNewDocs() {
         client.commit();
     });
 }
+
+
+Array.prototype.unique = function() {
+    var o = {}, i, l = this.length, r = [];
+    for(i=0; i<l;i+=1) o[this[i]] = this[i];
+    for(i in o) r.push(o[i]);
+    return r;
+};
